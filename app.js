19f67de3a602a113f6362f89200bd367ae129086
hakan <hakanyakarcomeng@gmail.com>
2018-12-09 12:36:50 +0000

    mongolab

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://yelpcamp:h1a9k9o4@ds263137.mlab.com:63137/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
